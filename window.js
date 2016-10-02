8390efa1935d53f5b8a3e81739d8e99206bebba9
Cartmanez64 <cartmanez64@gmail.com>
2016-10-03 00:23:38 +0200

    Ajout de fonctionnalités
    - déplacer la fenêtre
    - gestion de plusieurs fenêtre sur la page

--- window.js
+++ window.js
@@ -1,6 +1,11 @@
 
 ;(function(){
-	
+
+	var dragOffset = {
+		x : 0,
+		y : 0
+	};
+
 	this.WindowJS = function(){
 		this._options = null;
 		this._window = null;
@@ -9,12 +14,15 @@
 		this._closeButton = null;
 		this._maximizeButton = null;
 		this._minimizeButton = null;
+
+		this._instanceId;
 		
 		var defaults = {
 			width: 700,
 			height: 500,
 			title: 'Title Here !',
 			modal: false,
+			draggable: false,
 			controls: true,
 			overlayColor: '#ffffff', /* #3fd or #33ffdd */
 			overlayOpacity: 90,
@@ -35,7 +43,24 @@
 	    {
 	    	this._options = defaults;
 	    }
+
+	    /*
+	     * Increment "instances" attribute to count number of WindowJS instances 
+	     * (instances attribute is decremented on close function of WindowJS)
+	     */
+		WindowJS.prototype.instances = WindowJS.prototype.instances ? WindowJS.prototype.instances + 1 : 1;
+
+		this._instanceId = WindowJS.prototype.instances;
+
+		/*
+		 * Sotre All instances in an array
+		 */
+		WindowJS.allInstances.push(this);
+
+		console.dir(WindowJS.allInstances);
 	}
+
+	WindowJS.allInstances = [];
 	
 	/**
 	 * Public Methods
@@ -43,15 +68,120 @@
 	WindowJS.prototype.open = function(){
 		buildWindow.call(this);
 		initEvents.call(this);
+		if(this._options.draggable)
+		{
+			draggable.call(this);
+		}
 	}
 	WindowJS.prototype.close = function(){
 		this._window.parentNode.removeChild(this._window);
-		this._overlay.parentNode.removeChild(this._overlay);
+
+		if(this._options.modal)
+		{
+			this._overlay.parentNode.removeChild(this._overlay);
+		}
+		
+		/* Decrement "instances" attribut */
+		WindowJS.prototype.instances--;
+
+		/* Remove instance from allInstances array */
+		var listToDelete = [this._instanceId];
+		var newArray = WindowJS.allInstances.filter(function(obj) {
+			return listToDelete.indexOf(obj._instanceId) === -1;
+		});
+
+		WindowJS.allInstances = newArray;
 	}
 	
 	/**
 	 * Private Methods
 	 */
+	function draggable()
+	{
+		var _this = this;
+
+
+		this._title.style.cursor = 'move';
+
+		this._title.onmousedown = mouseDown.bind(_this);
+		this._title.onmouseup = mouseUp.bind(_this);
+	}
+
+	function mouseDown(e)
+	{
+		var _this = this;
+	    //document.addEventListener('mousemove', mouseMove.bind(_this), false);
+	    document.onmousemove = mouseMove.bind(_this);
+
+	    e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+	    e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+	    dragOffset.x = e.pageX - _this._window.offsetLeft;
+	    dragOffset.y = e.pageY - _this._window.offsetTop;
+
+	    /* */
+	    WindowJS.allInstances.forEach(function(elem){
+	    	elem._window.style.zIndex = 9999 - elem._instanceId;
+	    });
+	    _this._window.style.zIndex = 9999 + _this._instanceId;
+	}
+
+	function mouseUp()
+	{
+		var _this = this;
+
+	    document.onmousemove = null;
+
+	    /* Reset the window opacity when moving is stopped */
+		_this._window.style.opacity = '1';
+
+		//console.log(_this._options.title);
+	}
+
+	function mouseMove(e)
+	{
+		var _this = this;
+
+		/* Reduce the window opacity during moving */
+		_this._window.style.opacity = '0.75';
+
+	    e.pageX = e.pageX || e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+	    e.pageY = e.pageY || e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+	    
+	    var offsetX, offsetY;
+
+	    // left/right constraints
+	    if (e.pageX - dragOffset.x < window.pageXOffset)
+	    {
+	        offsetX = window.pageXOffset;
+	    }
+	    else if (e.pageX - dragOffset.x + _this._window.offsetWidth > window.innerWidth)
+	    {
+	        offsetX = window.innerWidth - _this._window.offsetWidth;
+	    }
+	    else
+	    {
+	        offsetX = e.pageX - dragOffset.x;
+	    }
+	     
+	    // top/bottom constraints
+	    if (e.pageY - dragOffset.y < window.pageYOffset)
+	    {
+	        offsetY = window.pageYOffset;
+	    }
+	    else if (e.pageY - dragOffset.y + _this._window.offsetHeight > window.innerHeight)
+	    {
+	        offsetY = window.innerHeight - _this._window.offsetHeight;
+	    }
+	    else
+	    {
+	        offsetY = e.pageY - dragOffset.y;
+	    }  
+
+
+	    _this._window.style.top = offsetY + 'px';
+	    _this._window.style.left = offsetX + 'px';
+	}
+
 	function buildWindow()
 	{
 		var titleBar, controlsContainer, content, contentContainer, docFrag;
@@ -59,7 +189,6 @@
 		docFrag = document.createDocumentFragment();
 		
 		this._window = document.createElement('div');
-		this._overlay = document.createElement('div');
 		titleBar = document.createElement('div');
 		
 		this._closeButton = document.createElement('i');
@@ -94,12 +223,7 @@
 		
 		/* Window */
 		this._window.className = 'window';
-		
-		/* Window Overlay */
-		this._overlay.className = 'w-overlay';
-		this._overlay.style.backgroundColor = this._options.overlayColor
-		this._overlay.style.opacity = cssOpacity(this._options.overlayOpacity);
-		
+				
 		/* Window Content Container */
 		contentContainer.className = 'w-content';
 		
@@ -109,6 +233,7 @@
 			{
 				if(this._options.ajax.method == 'get')
 				{
+					
 					var request = new XMLHttpRequest();
 					request.open('GET', this._options.ajax.url, true);
 
@@ -117,9 +242,11 @@
 							// Check if the get was successful.
 							if (request.status === 200) {
 								contentContainer.innerHTML = request.responseText;	
+								contentContainer.querySelectorAll('script').forEach(function(elem){
+									eval(elem.innerText);
+								});
 							} else {
-								console.error(request.statusText);
-								contentContainer.innerHTML = request.statusText;	
+								contentContainer.innerHTML = request.statusText;
 							}
 						}
 					};
@@ -129,6 +256,7 @@
 					};
 					
 					request.send(null);
+					
 					
 					/*
 					$.ajax({
@@ -144,6 +272,8 @@
 						}
 					});
 					*/
+
+					//contentContainer.innerHTML = '<object type="text/html" data="' + this._options.ajax.url +'" ></object>';
 					
 				}
 				else if(this._options.ajax.method == 'post')
@@ -164,8 +294,24 @@
 		this._window.appendChild(titleBar);
 		this._window.appendChild(contentContainer);
 		
+		/* If is a modal window */
+		if(this._options.modal)
+		{
+			/* Window Overlay */
+			this._overlay = document.createElement('div');
+			this._overlay.className = 'w-overlay';
+
+			/* Window Overlay Style */
+			this._overlay.style.backgroundColor = this._options.overlayColor
+			this._overlay.style.opacity = cssOpacity(this._options.overlayOpacity);
+			/* Place the overlay in the foreground (just behind the window) */
+			this._overlay.style.zIndex = this.instances + 9998;
+
+			/* Document fragement */
+			docFrag.appendChild(this._overlay);
+		}
+
 		/* Document fragement */
-		docFrag.appendChild(this._overlay);
 		docFrag.appendChild(this._window);
 		
 		/* Append document fragment to DOM */
@@ -178,7 +324,11 @@
 		
 		this._window.style.width = this._options.width - (borderWidth * 2) + 'px';
 		this._window.style.height = this._options.height - (borderWidth * 2) + 'px';
-		
+		this._window.style.left = window.innerWidth / 2 - this._window.offsetWidth / 2 + 'px';
+		this._window.style.top = window.innerHeight / 2 - this._window.offsetHeight / 2 + 'px';
+		/* Place the window in the foreground */
+	    this._window.style.zIndex = this.instances + 9999;
+
 		/* Title Style */
 		var titleStyle = this._title.currentStyle || window.getComputedStyle(this._title);
 		var paddingLeft = titleStyle.paddingLeft.substr(0, titleStyle.paddingLeft.length - 2)
@@ -193,10 +343,10 @@
 			this._closeButton.addEventListener('click', this.close.bind(this));
 		}
 		
-		if(this._overlay)
+		/*if(this._overlay)
 		{
 			this._overlay.addEventListener('click', this.close.bind(this));
-		}
+		}*/
 	}
 	
 	/* Utility method to extend defaults with user options */
